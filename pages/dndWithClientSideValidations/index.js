7473303b45321202d46ed981f533de1a17fced33
AW-Synoptek <awood@synoptek.com>
2022-12-30 12:53:06 -0800

    Started LeftComponentPanel and new test page

--- pages/dndWithClientSideValidations/index.js
+++ pages/dndWithClientSideValidations/index.js
@@ -1,5 +1,8 @@
 import { DndContext } from '@dnd-kit/core'
 import Head from 'next/head'
+import { Card } from 'primereact/card'
+import DndLeftPanel from '../../components/DndComponents/DndLeftPanel'
+import { Droppable } from '../../components/DndComponents/Droppable'
 
 export default function DndWithClientSideValidations() {
     return (
@@ -9,7 +12,14 @@ export default function DndWithClientSideValidations() {
                 <link rel='icon' sizes='32x32' href='/component-library/logo.png' />
             </Head>
             <DndContext>
-                
+                <div className='grid'>
+                    <DndLeftPanel />
+                    <Card className='card form-horizontal mt-5' style={{'width': '70%'}}>
+                        {/* <Droppable>
+                            
+                        </Droppable> */}
+                    </Card>
+                </div>
             </DndContext>
         </>
     )
