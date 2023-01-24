9e6b7bdb211fadbe1a8d6fba4db08597becf0c9b
AW-Synoptek <awood@synoptek.com>
2023-01-24 12:33:39 -0800

    Split handleDragEnd into three separate functions

--- hooks/useDnd.js
+++ hooks/useDnd.js
@@ -23,65 +23,53 @@ const useDnd = () => {
         }
     }
 
-    const handleDragEnd = (event, metadata, addMetadata, setMetadata, setMainFormIds, mainFormIds, dragOverCapture) => {
-        const { active, over } = event
-
-        if (dragOverCapture.current) {
-            if (typeof dragOverCapture.current.id !== 'number') {
-                if (dragOverCapture.current.id.includes('section')) {
-                    dragOverCapture.current.id = Number(dragOverCapture.current.id.substring(dragOverCapture.current.id.length - 1))
-                    // Grabs number at the end of 'section-' which is required for the code below to find the position of the section component.
-                }
-            }
-
-            setMetadata(prevState => {
-                const indexOfSection = mainFormIds.findIndex(element => element == dragOverCapture.current?.id)
-
-                if (indexOfSection === -1) return prevState // Prevents error being thrown when indexOfSection returns -1.
-
-                const tempMetadata = JSON.parse(JSON.stringify(prevState))
-
-                dragOverCapture.current.componentData.id = `section-${indexOfSection + 1}_${tempMetadata[indexOfSection].sectionMetadata.length + 1}`
-                dragOverCapture.current.componentData.name = `${dragOverCapture.current.componentData.name}-${Guid.newGuid()}`
-
-                tempMetadata[indexOfSection].sectionMetadata.push(dragOverCapture.current.componentData)
-
-                return tempMetadata
-            })
-
-            dragOverCapture.current = null
-
-            return
-        }
-
-        if (typeof active.id === 'string') { // This nested if is for sorting components within a section.
-            if (active.id.includes('section')) {
-                if (active.id !== over.id) {
-                    const sectionNumber = active.id.substring(0, 9)
-
-                    setMetadata(prevState => {
-                        let tempMetadata = prevState.slice(0)
-                        const sectionIndex = tempMetadata.findIndex(element => element.name === sectionNumber)
-                        let tempSectionMetadata = tempMetadata[sectionIndex].sectionMetadata.slice(0)
-                        const movingComponentId = tempMetadata[sectionIndex].sectionMetadata.findIndex(element => element.id === active.id)
-                        const newPositionId = tempMetadata[sectionIndex].sectionMetadata.findIndex(element => element.id === over.id)
-                        const movingComponent = tempMetadata[sectionIndex].sectionMetadata.slice(movingComponentId, movingComponentId + 1 ? movingComponentId + 1 : null)
-
-                        tempSectionMetadata.splice(movingComponentId, 1)
-                        tempSectionMetadata.splice(newPositionId, 0, ...movingComponent)
-                        
-                        tempMetadata[sectionIndex].sectionMetadata = tempSectionMetadata
-
-                        return tempMetadata
-
-                    })
-                }
-                return
+    const addComponentInSection = (dragOverCapture, setMetadata, mainFormIds) => {
+        if (typeof dragOverCapture.current.id !== 'number') {
+            if (dragOverCapture.current.id.includes('section')) {
+                dragOverCapture.current.id = Number(dragOverCapture.current.id.substring(dragOverCapture.current.id.length - 1))
+                // Grabs number at the end of 'section-' which is required for the code below to find the position of the section component.
             }
         }
 
-        if (over !== null && !active.data.current.sortable) {
-            const updatedData = JSON.parse(JSON.stringify(active.data.current))
+        setMetadata(prevState => {
+            const indexOfSection = mainFormIds.findIndex(element => element == dragOverCapture.current?.id)
+
+            if (indexOfSection === -1) return prevState // Prevents error being thrown when indexOfSection returns -1.
+
+            const tempMetadata = JSON.parse(JSON.stringify(prevState))
+
+            dragOverCapture.current.componentData.id = `section-${indexOfSection + 1}_${tempMetadata[indexOfSection].sectionMetadata.length + 1}`
+            dragOverCapture.current.componentData.name = `${dragOverCapture.current.componentData.name}-${Guid.newGuid()}`
+
+            tempMetadata[indexOfSection].sectionMetadata.push(dragOverCapture.current.componentData)
+
+            return tempMetadata
+        })
+    }
+
+    const sortComponentInSection = (active, over, setMetadata) => {
+        const sectionNumber = active.id.substring(0, 9)
+
+        setMetadata(prevState => {
+            let tempMetadata = prevState.slice(0)
+            const sectionIndex = tempMetadata.findIndex(element => element.name === sectionNumber)
+            let tempSectionMetadata = tempMetadata[sectionIndex].sectionMetadata.slice(0)
+            const movingComponentId = tempMetadata[sectionIndex].sectionMetadata.findIndex(element => element.id === active.id)
+            const newPositionId = tempMetadata[sectionIndex].sectionMetadata.findIndex(element => element.id === over.id)
+            const movingComponent = tempMetadata[sectionIndex].sectionMetadata.slice(movingComponentId, movingComponentId + 1 ? movingComponentId + 1 : null)
+
+            tempSectionMetadata.splice(movingComponentId, 1)
+            tempSectionMetadata.splice(newPositionId, 0, ...movingComponent)
+            
+            tempMetadata[sectionIndex].sectionMetadata = tempSectionMetadata
+
+            return tempMetadata
+
+        })
+    }
+
+    const addMainForm = (active, metadata, addMetadata) => {
+        const updatedData = JSON.parse(JSON.stringify(active.data.current))
 
             if (updatedData.type === 'section') {
                 if (metadata.length === 0) {
@@ -100,28 +88,57 @@ const useDnd = () => {
             }
 
             addMetadata(updatedData)
+    }
+
+    const sortMainForm = (active, over, setMainFormIds, setMetadata) => {
+        setMainFormIds(ids => {
+            const activeIndex = ids.indexOf(active.id)
+            const overIndex = ids.indexOf(over.id)
+
+            return arrayMove(ids, activeIndex, overIndex)
+        })
+
+        setMetadata(prevState => {
+            const movingComponentId = active.id - 1
+            const newPositionId = over.id - 1
+            let tempMetadata = prevState.slice(0)
+
+            const movingComponent = tempMetadata.slice(movingComponentId, movingComponentId + 1 ? movingComponentId + 1 : null)
+            tempMetadata.splice(movingComponentId, 1)
+            tempMetadata.splice(newPositionId, 0, ...movingComponent)
+
+            return tempMetadata
+        })
+    }
+
+    const handleDragEnd = (event, metadata, addMetadata, setMetadata, setMainFormIds, mainFormIds, dragOverCapture) => {
+        const { active, over } = event
+
+        if (dragOverCapture.current) {
+            addComponentInSection(dragOverCapture, setMetadata, mainFormIds)
+
+            dragOverCapture.current = null
+
+            return
+        }
+
+        if (typeof active.id === 'string') { // This nested if is for sorting components within a section.
+            if (active.id.includes('section')) {
+                if (active.id !== over.id) {
+                    sortComponentInSection(active, over, setMetadata)
+                }
+
+                return
+            }
+        }
+
+        if (over !== null && !active.data.current.sortable) {
+            addMainForm(active, metadata, addMetadata)
         }
 
         if (active.data.current.sortable) {
             if (active.id !== over.id) {
-                setMainFormIds(ids => {
-                    const activeIndex = ids.indexOf(active.id)
-                    const overIndex = ids.indexOf(over.id)
-
-                    return arrayMove(ids, activeIndex, overIndex)
-                })
-
-                setMetadata(prevState => {
-                    const movingComponentId = active.id - 1
-                    const newPositionId = over.id - 1
-                    let tempMetadata = prevState.slice(0)
-
-                    const movingComponent = tempMetadata.slice(movingComponentId, movingComponentId + 1 ? movingComponentId + 1 : null)
-                    tempMetadata.splice(movingComponentId, 1)
-                    tempMetadata.splice(newPositionId, 0, ...movingComponent)
-
-                    return tempMetadata
-                })
+                sortMainForm(active, over, setMainFormIds, setMetadata)
             }
         }
     }
