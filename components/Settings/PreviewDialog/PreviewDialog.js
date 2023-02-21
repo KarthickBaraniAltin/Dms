2ccd7d18e7cecd860a4c4277ee0c11d422efb8b3
AW-Synoptek <awood@synoptek.com>
2023-02-21 15:03:08 -0800

    Removed usePreviewCreator hook

--- components/Settings/PreviewDialog/PreviewDialog.js
+++ components/Settings/PreviewDialog/PreviewDialog.js
@@ -1,16 +1,8 @@
 import { Dialog } from "primereact/dialog"
-import { usePreviewCreator } from "../../../hooks/usePreviewCreator"
-import { Card } from "primereact/card"
 import { useRenderItems } from "../../../hooks/useRenderItems"
 
 export default function PreviewDialog({ showDialog, handlePreview, metadata, setMetadata, headerImage, handleHeaderImage}) {
-
-    let labelList = []
-    let inputFieldList = []
-    let subtitleList = []
     let componentList = []
-    // const { renderPreview } = usePreviewCreator({ metadata })
-    // const components = renderPreview()?.props?.children
     const { renderLabel, renderCreateElements, renderSubtitle } = useRenderItems({ metadata, setMetadata, headerImage, handleHeaderImage })
 
     metadata.map(element => {
@@ -57,64 +49,6 @@ export default function PreviewDialog({ showDialog, handlePreview, metadata, set
             </div>
         )
     })
-
-    // components.map(component => {
-    //     if (component.props.children.length === 2) { // Checks to see if component is section component.
-    //         labelList.push(component.props.children[0])
-    //         inputFieldList.push(component.props.children[1])
-    //         subtitleList.push(null) // Required for subtitle to appear in non-section components.
-    //         return
-    //     }
-    //     labelList.push(component.props.children.props.children[0])
-    //     inputFieldList.push(component.props.children.props.children[1])
-    //     subtitleList.push(component.props.children.props.children[3])
-    // })
-
-    // for (let i = 0; i < components.length; i++) {
-    //     if (metadata[i].type === 'header') {
-    //         console.log('made it')
-    //         componentList.push(
-    //             <div>
-    //                 {/* <Card style={{'background': '#004990', 'color': 'white', 'marginBottom': '0.5rem'}}>
-    //                     <h1 style={{'textAlign': 'center'}}>{metadata[i].label}</h1>
-    //                 </Card> */}
-                    
-    //                 {renderLabel(headerImage, metadata[i].label, metadata[i].type, true, true)}
-    //             </div>
-    //         )
-    //     }
-
-    //     if (components[i].props.children.length === 2) { // Checks to see if component is section component.
-    //         componentList.push(
-    //             <div>
-    //                 <h3>
-    //                     {labelList[i]}
-    //                 </h3>
-    //                 {inputFieldList[i].map(element => {
-    //                     return (
-    //                         <div key={i} style={{display: 'flex', gap: '2rem', marginBottom: '2rem'}}>
-    //                             <div style={{width: '100px'}}>
-    //                                 {element.props.children[0]}
-    //                                 {element.props.children[2]}
-    //                             </div>
-    //                             {element.props.children[1]}
-    //                         </div>
-    //                     )
-    //                 })}
-    //             </div>
-    //         )
-    //         continue
-    //     }
-    //     componentList.push(
-    //         <div key={i} style={{display: 'flex', gap: '2rem', marginBottom: '1rem'}}>
-    //             <div style={{width: '100px'}}>
-    //                 {labelList[i]}
-    //                 {subtitleList[i]}
-    //             </div>
-    //             {inputFieldList[i]}
-    //         </div>
-    //     )
-    // }
 
     return (
         <>
