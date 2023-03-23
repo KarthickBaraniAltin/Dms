5e9918e829c1788eab629aec261caa36d4b4b87d
AW-Synoptek <awood@synoptek.com>
2023-03-23 14:41:26 -0700

    Removed dropdown from ViewSignature

--- components/ViewComponents/Inputs/ViewSignature/ViewSignature.js
+++ components/ViewComponents/Inputs/ViewSignature/ViewSignature.js
@@ -9,17 +9,6 @@ import Subtitle from '../../../SharedComponents/Subtitle/Subtitle'
 
 export default function ViewSignature ({metadata, value, onChange, errors }) {
     const { name, label, subtitle, guid, fontStyle } = metadata
-    const { handleSignatureChange, fontInputs } = useSignatureInputs()
-    const fontValue = fontInputs.find(obj => obj.name === name)
-
-    const fontOptions = [
-        {label: 'Times New Roman', value: 'Times New Roman'},
-        {label: 'Arial', value: 'Arial'},
-        {label: 'Georgia', value: 'Georgia'},
-        {label: 'Cursive', value: 'Cursive'},
-        {label: 'Calibri' , value: 'Calibri'},
-        {label: 'Tangerine', value: 'Tangerine'}
-    ]
 
     return (
         <div className='field grid grid-nogutter'>
@@ -29,7 +18,6 @@ export default function ViewSignature ({metadata, value, onChange, errors }) {
             </div>
             <div className='grid grid-nogutter col-8'>
                 <InputText className={`col-8 ${styles.input}`} name={name} value={value} onChange={onChange} style={{ fontSize: '1rem', fontFamily: fontStyle}}/>
-                <Dropdown className='col-4' placeholder='Fonts' name='fonts' value={fontValue?.value} options={fontOptions} onChange={event => handleSignatureChange(event, guid, metadata)} />
             </div>
             <div className='col-12'>
                 <p style={{border: '2px solid #004990', padding: '0.5rem', marginRight: '0.5rem', fontFamily: fontStyle}}>{value}</p>
