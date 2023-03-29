b3a61dd75f6c555ff155437f5817844ae4fa599a
Ahmet Cengiz <ahmet_fatih_cengiz@hotmail.com>
2023-03-29 11:09:21 -0400

    Don't use Logo

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import Logo from './Logo'
 import { AuthenticatedTemplate, UnauthenticatedTemplate, useMsal } from "@azure/msal-react"
 import { Menubar } from 'primereact/menubar'
 import { Button } from 'primereact/button'
@@ -23,17 +22,16 @@ export default function Navbar() {
     }
   ]
 
-  const start = <Logo />
   const logoutButton = <Button label='logout' className='btn btn-primary mr-3' onClick={() => instance.logoutPopup()} />
   const loginButton = <Button label='Sign In' className='btn btn-primary mr-3' onClick={() => instance.loginPopup()} />
   
   return (
       <>
         <AuthenticatedTemplate>
-          <Menubar className='-m-2' start={<Logo />} end={logoutButton} style={{'backgroundColor': '#004990', 'boxShadow': '0 0 11px #0003', border: 'none'}} />
+          <Menubar className='-m-2' end={logoutButton} style={{'backgroundColor': '#004990', 'boxShadow': '0 0 11px #0003', border: 'none'}} />
         </AuthenticatedTemplate>
         <UnauthenticatedTemplate>
-          <Menubar className='-m-2' start={<Logo />} end={loginButton} style={{'backgroundColor': '#004990', 'boxShadow': '0 0 11px #0003', border: 'none'}} />
+          <Menubar className='-m-2' end={loginButton} style={{'backgroundColor': '#004990', 'boxShadow': '0 0 11px #0003', border: 'none'}} />
         </UnauthenticatedTemplate>
       </>
   )
