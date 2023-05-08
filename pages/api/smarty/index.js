b71cc8b59da163146a0554721df26736cb794f50
AW-Synoptek <awood@synoptek.com>
2023-05-08 09:42:09 -0700

    Updated auth credentials for Smarty API

--- pages/api/smarty/index.js
+++ pages/api/smarty/index.js
@@ -2,8 +2,10 @@ import SmartySDK from "smartystreets-javascript-sdk"
 const SmartyCore = SmartySDK.core
 const Lookup = SmartySDK.usAutocompletePro.Lookup
 
-let authId = '2cdff039-0fc8-18ef-2759-1b4c3f0f898c'
-let authToken = 'eE5LVc4z71roT8L4S0i9'
+// let authId = '2cdff039-0fc8-18ef-2759-1b4c3f0f898c'
+let authId = "c79e1839-2e38-078d-c125-7cc441d1316c"
+// let authToken = 'eE5LVc4z71roT8L4S0i9'
+let authToken = "JqrH7UjRHXuRHSorQxRH"
 
 let clientBuilder = new SmartyCore.ClientBuilder(new SmartyCore.StaticCredentials(authId, authToken))
 let client = clientBuilder.buildUsAutocompleteProClient()
