cf052498b2f0f872a10a14a3628981f51d5a6bbc
harishk16 <harishk01621@gmail.com>
2023-06-03 08:25:48 +0000

    Demo_Changes

--- pages/document/index.js
+++ pages/document/index.js
@@ -4,53 +4,36 @@ import Flex from "../../components/Layout/Flex"
 import Header from "../../components/Header/Header"
 import TextInput from "../../components/Input/TextInput"
 import { useRouter } from "next/router"
-import { Image } from 'primereact/image';
+
+import Forms from '../../svg/Forms.svg';
+import batch from '../../svg/batch.svg';
+import Image from 'next/image';
 
 
-
-         
 export default function Home() {
 
     const router = useRouter()
-
+    console.log(Forms)
 
     return (
         <>
-         <div className="card flex justify-content-center">
-            <Image src="https://primefaces.org/cdn/primereact/images/galleria/galleria7.jpg"  alt="Image" width="250" />
-        </div>
+            <div className="card flex justify-content-center">
+
+
+            </div>
             <Flex direction={'column'} className={'my-4 h-full w-full bg-white justify-content-start border-round'} >
                 <Flex className={'px-3 py-2'} >
                     <Header size={2}>Document</Header>
                 </Flex>
-                <Flex direction={'column'} className={'justify-content-center align-items-center h-full'} >
-                    <Flex direction={'column'} className={'justify-content-center align-items-center gap-3 w-7'} >
-                        <Header size={2} className={'text-left text-primary'} >Would You Like To...</Header>
-                        <Button className={'btn btn-flex border-round-lg border-1 p-3 w-10'} onClick={() => {
-                            router.push('document/upload')
-                        }} >
-                            <Flex direction={'column'} className={'align-items-start'} >
-                                <Header size={2} >
-                                    Upload Forms of the Same Type
-                                </Header>
-                                <div>
-                                    <strong>Upload Or Scan documents and process them one by one</strong>
-                                </div>
-                            </Flex>
-                        </Button>
-                        <Button className={'btn btn-flex border-round-lg border-1 p-3 w-10'} >
-                            <Flex direction={'column'} className={'align-items-start'} >
-                                <Header size={2} >
-                                    Upload A Batch
-                                </Header>
-                                <div>
-                                    <strong>Upload Or Scan documents and process them one by one</strong>
-                                </div>
-                            </Flex>
-                        </Button>
-                    </Flex>
+                <Flex direction={'column'} className={'justify-content-between align-items-center h-30rem '} >
+                    <Header size={2} className={'text-left text-primary'} >Would You Like To...</Header>
+                    <Image src={Forms} height={550} width={550} onClick={() => {
+                        router.push('document/upload')
+                    }} ></Image>
+                    <Image src={batch} height={550} width={550} />
                 </Flex>
             </Flex>
+
 
         </>
     )
